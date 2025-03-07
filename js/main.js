c7314f5d8658fe958ce6bfdd3400c2e3fb089527
Michelle Burton <msmichelleburton@yahoo.com>
2025-03-07 10:47:48 -0600

    WiDriveOn Repo Setup

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+const representatives = {
+        "53223": { repName: "Rep. Russell A. Goodwin", repEmail: "Rep.Goodwin@legis.wisconsin.gov" },
+        "53002": { repName: "Rep. Jane Smith", repEmail: "janesmith@example.com" },
+        // Add more ZIP Codes and representatives here
+    };
+
+function updateRepresentativeInfo() {
+    const userFirstName = document.getElementById('userFirstName').value;
+    const userLastName = document.getElementById('userLastName').value;
+    const userZipCode = document.getElementById('userZipCode').value;
+    const representativeInfo = representatives[userZipCode];
+
+    document.getElementById('repName').textContent = representativeInfo ? representativeInfo.repName : 'Unknown';
+    document.getElementById('repEmail').textContent = representativeInfo ? representativeInfo.repEmail : 'Unknown';
+
+    document.getElementById('userFirstNameDisplay').textContent = userFirstName;
+    document.getElementById('userLastNameDisplay').textContent = userLastName;
+    document.getElementById('userZipCodeDisplay').textContent = userZipCode;
+}
